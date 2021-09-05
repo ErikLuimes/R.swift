6448e2eeeb6c4a8ee059804b899e32b9e0ae7f8e
Erik Luimes <erik.luimes@gmail.com>
2021-09-05 13:30:35 +0200

    Rename target

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-  name: "RSwift",
+  name: "Rswift",
   platforms: [
     .macOS(.v10_11)
   ],
   products: [
-    .executable(name: "rswift", targets: ["RSwift"])
+    .executable(name: "rswift", targets: ["Rswift"])
   ],
   dependencies: [
     .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
     .package(url: "https://github.com/tomlokhorst/XcodeEdit", from: "2.7.0")
   ],
   targets: [
-    .target(name: "RSwift", dependencies: ["RswiftCore"]),
+    .target(name: "Rswift", dependencies: ["RswiftCore"]),
     .target(name: "RswiftCore", dependencies: ["Commander", "XcodeEdit"]),
     .testTarget(name: "RswiftCoreTests", dependencies: ["RswiftCore"]),
   ]
